6e10ed401c5bc2d6dbe9c8fc5d304037ea909f47
Denis Frenademetz <denisfrenademetz97@gmail.com>
2023-04-06 10:24:42 +0200

    fix: upgrade nx (#95)

--- jest.preset.js
+++ jest.preset.js
@@ -1,3 +1,6 @@
 const nxPreset = require('@nrwl/jest/preset').default;
 
-module.exports = { ...nxPreset, coverageReporters: ['html', 'json'] };
+module.exports = {
+    ...nxPreset,
+    coverageReporters: ['html', 'json'],
+};
